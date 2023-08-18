0ddaee7ee590810f46ba45aa496eb70712f515a6
vjsmit <vj.smit97@gmail.com>
2023-08-18 23:31:53 +0530

    Build all app components

---
+++ vars/mainci.groovy
@@ -0,0 +1,45 @@
+def call() {
+    pipeline {
+
+        agent {
+            node { label 'workstation' }
+        }
+
+        stages {
+
+            if (env.cibuild == "java") {
+
+                stage ('Build') {
+                    steps {
+                        sh 'mvn package'
+                    }
+                }
+            }
+
+            stage ('Unit-Tests') {
+                steps {
+                    echo 'Unit-Tests'
+                }
+            }
+
+            stage ('Code-Analysis') {
+                steps {
+                    echo 'sonar'
+                    //sh 'sonar-scanner -Dsonar.host.url=http://172.31.92.0:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
+                }
+            }
+
+            stage ('Security Checks') {
+                steps {
+                    echo 'Security Checks'
+                }
+            }
+
+            stage ('Publish Artifact') {
+                steps {
+                    echo 'Publish Artifact'
+                }
+            }
+        }
+    }
+}
