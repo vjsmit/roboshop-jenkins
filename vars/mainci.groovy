c0709d802b181c083b3663134923e3759ed08c85
vjsmit <vj.smit97@gmail.com>
2023-08-19 18:23:31 +0530

    Build all app components

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -1,12 +1,12 @@
 def call() {
 
-    node ('workstation') {
+    node('workstation') {
 
-        stage ('Code-Checkout') {
+        stage('Code-Checkout') {
 
             sh 'find . | grep "^./" |xargs rm -rf'
 
-            if(env.TAG_NAME ==~ ".*") {
+            if (env.TAG_NAME ==~ ".*") {
                 env.gitbrname = "refs/tags/${env.TAG_NAME}"
             } else {
                 env.gitbrname = "${env.BRANCH_NAME}"
@@ -16,32 +16,31 @@ def call() {
 
         if (env.cibuild == "java") {
 
-            stage ('Build') {
+            stage('Build') {
                 sh 'mvn package'
             }
         }
 
-        stage ('Unit-Tests') {
+        stage('Unit-Tests') {
             echo 'Unit-Tests'
         }
 
-        stage ('Code-Analysis') {
+        stage('Code-Analysis') {
             echo 'sonar'
             //sh 'sonar-scanner -Dsonar.host.url=http://172.31.92.0:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
         }
 
-        stage ('Security Checks') {
+        stage('Security Checks') {
             echo 'Security Checks'
         }
 
         if (env.TAG_NAME ==~ ".*") {
-            stage ('Publish Artifact') {
-                if (env.cibuild == "nginx") {
-                    sh 'zip -r ${component}-{TAG_NAME}.zip *'
-                }
-            echo 'Publish Artifact'
+            stage('Publish Artifact') {
+                sh 'rm -rf Jenkinsfile'
+                sh 'echo ${TAG_NAME} >VERSION'
+                sh 'zip -r ${component}-{TAG_NAME}.zip *'
+                sh 'curl -v -u admin:admin123 --upload-file ${component}-{TAG_NAME}.zip http://172.31.92.253:8081/repository/${component}/${component}-{TAG_NAME}.zip'
             }
-            sh 'curl -v -u admin:admin123 --upload-file ${component}-{TAG_NAME}.zip http://172.31.92.253:8081/repository/${component}/${component}-{TAG_NAME}.zip'
         }
     }
 }
