d55e76c3ede916cd694af310f9674a8f3e0156d8
vjsmit <vj.smit97@gmail.com>
2023-08-19 18:42:26 +0530

    Build all app components

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -39,7 +39,7 @@ def call() {
                 sh 'rm -f Jenkinsfile'
                 sh 'echo ${TAG_NAME} >VERSION'
                 sh 'zip -r ${component}-${TAG_NAME}.zip *'
-                sh 'curl -v -u admin:admin123 --upload-file ${component}-{TAG_NAME}.zip http://172.31.92.253:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.92.253:8081/repository/${component}/${component}-${TAG_NAME}.zip'
             }
         }
     }
