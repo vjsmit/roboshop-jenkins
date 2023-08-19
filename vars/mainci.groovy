7d2fef548ddc86c261e4120e7bda2e9d791e57c7
vjsmit <vj.smit97@gmail.com>
2023-08-19 18:40:32 +0530

    Build all app components

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -36,10 +36,10 @@ def call() {
 
         if (env.TAG_NAME ==~ ".*") {
             stage('Publish Artifact') {
-                sh 'rm -rf Jenkinsfile'
+                sh 'rm -f Jenkinsfile'
                 sh 'echo ${TAG_NAME} >VERSION'
-                sh 'zip -r ${component}-{TAG_NAME}.zip *'
-                sh 'curl -v -u admin:admin123 --upload-file ${component}-{TAG_NAME}.zip http://172.31.92.253:8081/repository/${component}/${component}-{TAG_NAME}.zip'
+                sh 'zip -r ${component}-${TAG_NAME}.zip *'
+                sh 'curl -v -u admin:admin123 --upload-file ${component}-{TAG_NAME}.zip http://172.31.92.253:8081/repository/${component}/${component}-${TAG_NAME}.zip'
             }
         }
     }
